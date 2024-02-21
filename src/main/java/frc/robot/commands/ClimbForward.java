58d8c57c6e8f44bc8fa25c9ae0fd7ace695c9652
= <=>
2024-02-20 21:40:45 -0500

    clean up

--- src/main/java/frc/robot/commands/ClimbForward.java
+++ src/main/java/frc/robot/commands/ClimbForward.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.ConveyorSubsystem;
 
 public class ClimbForward extends Command {
 Climber climber;
